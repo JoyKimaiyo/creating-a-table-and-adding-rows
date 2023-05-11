fbc83226adac7b814ddf2820fe8b39baecab4971
Joyce Kimaiyo <106971527+JoyKimaiyo@users.noreply.github.com>
2023-05-11 09:46:55 +0300

    Add files via upload

---
+++ SQLQuery10 creating a table and adding rows.sql
@@ -0,0 +1,4 @@
+CREATE TABLE goodreads_books 
+( id INTEGER, title VARCHAR(198) );
+INSERT INTO goodreads_books (id, title) 
+VALUES (1, 'The Great Gatsby'), (2, 'To Kill a Mockingbird'), (3, '1984');
